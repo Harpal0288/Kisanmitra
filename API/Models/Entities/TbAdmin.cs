d619597fbf720202b54c9b362e149c5f635820c8
Harpal2000 <rajanbabrah056@gmail.com>
2024-08-08 11:37:07 +0530

    initial commit for kisanmitra app

---
+++ API/Models/Entities/TbAdmin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Models.Entities;
+
+[Table("tb_Admin")]
+public partial class TbAdmin
+{
+    [Key]
+    [Column("admin_id")]
+    [StringLength(255)]
+    public string AdminId { get; set; } = null!;
+
+    [Column("user_id")]
+    [StringLength(255)]
+    public string? UserId { get; set; }
+
+    [Column("inserted_date", TypeName = "datetime")]
+    public DateTime InsertedDate { get; set; }
+
+    [Column("inserted_by")]
+    [StringLength(255)]
+    public string InsertedBy { get; set; } = null!;
+
+    [Column("updated_date", TypeName = "datetime")]
+    public DateTime UpdatedDate { get; set; }
+
+    [Column("updated_by")]
+    [StringLength(255)]
+    public string UpdatedBy { get; set; } = null!;
+
+    [ForeignKey("UserId")]
+    [InverseProperty("TbAdmins")]
+    public virtual TbUser? User { get; set; }
+}
---
+++ API/Models/Entities/TbPaymentGateway.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Models.Entities;
+
+[Table("tb_PaymentGateway")]
+public partial class TbPaymentGateway
+{
+    [Key]
+    [Column("gateway_id")]
+    [StringLength(255)]
+    public string GatewayId { get; set; } = null!;
+
+    [Column("gateway_name")]
+    [StringLength(255)]
+    public string GatewayName { get; set; } = null!;
+
+    [Column("gateway_details")]
+    [StringLength(255)]
+    public string GatewayDetails { get; set; } = null!;
+
+    [Column("inserted_date", TypeName = "datetime")]
+    public DateTime InsertedDate { get; set; }
+
+    [Column("inserted_by")]
+    [StringLength(255)]
+    public string InsertedBy { get; set; } = null!;
+
+    [Column("updated_date", TypeName = "datetime")]
+    public DateTime UpdatedDate { get; set; }
+
+    [Column("updated_by")]
+    [StringLength(255)]
+    public string UpdatedBy { get; set; } = null!;
+}
